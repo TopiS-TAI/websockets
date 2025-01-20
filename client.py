d7f74d8dd9cf1f1402a1ec8be537b29359769344
Topi Saavalainen <topi.saavalainen@edu.turku.fi>
2025-01-20 20:42:36 +0200

    Add client name

--- client.py
+++ client.py
@@ -1,14 +1,19 @@
 import asyncio
 import websockets
+import config
 
-async def chat():
-    async with websockets.connect('ws://localhost:12345') as websocket:
+async def chat(name):
+    async with websockets.connect(f'ws://{config.server_ip}:{config.server_port}/{name}') as websocket:
         while True:
             message = input("Message: ")
             await websocket.send(message)
             response = await websocket.recv()
             print(f"- {response}")
 
-if __name__ == '__main__':
+def main():
+    name = input("Enter name: ")
     print('Starting client')
-    asyncio.run(chat())
+    asyncio.run(chat(name))
+
+if __name__ == '__main__':
+    main()
--- server.py
+++ server.py
@@ -1,25 +1,28 @@
 import asyncio
 import websockets
+import config
+from pprint import pprint
 
 connected_clients = set()
 
 async def handle_client(websocket):
-    print('Got client', websocket)
+    print('Got client', websocket.request.path)
     connected_clients.add(websocket)
+    user = websocket.request.path[1:]
 
     try:
         async for message in websocket:
-            print(f'Received: {message}')
+            print(f'Received from {user}: {message}')
             for client in connected_clients:
                 if client != websocket:
-                    await client.send(message)
+                    await client.send(f'{user}: {message}')
     except websockets.exceptions.ConnectionClosed:
         pass
     finally:
         connected_clients.remove(websocket)
 
 async def main():
-    server = await websockets.serve(handle_client, 'localhost', 12345)
+    server = await websockets.serve(handle_client, config.server_ip, config.server_port)
     await server.wait_closed()
 
 if __name__ == '__main__':
