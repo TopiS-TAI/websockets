893b8d22d95c5225a85cd2e91ca1381478408331
Topi Saavalainen <topi.saavalainen@edu.turku.fi>
2025-01-20 21:35:02 +0200

    Added timeouts config

--- client.py
+++ client.py
@@ -3,12 +3,16 @@ import websockets
 import config
 
 async def chat(name):
-    async with websockets.connect(f'ws://{config.server_ip}:{config.server_port}/{name}') as websocket:
+    async with websockets.connect(
+        f'ws://{config.server_ip}:{config.server_port}/{name}',
+        ping_timeout=60,
+        ping_interval=10
+    ) as websocket:
         while True:
             message = input("Message: ")
             await websocket.send(message)
             response = await websocket.recv()
-            print(f"- {response}")
+            print(f" {response}")
 
 def main():
     name = input("Enter name: ")
--- server.py
+++ server.py
@@ -15,14 +15,22 @@ async def handle_client(websocket):
             print(f'Received from {user}: {message}')
             for client in connected_clients:
                 if client != websocket:
+                    print(f'sending to {client.request.path[1:]}')
                     await client.send(f'{user}: {message}')
+                    print('sent')
     except websockets.exceptions.ConnectionClosed:
         pass
     finally:
         connected_clients.remove(websocket)
 
 async def main():
-    server = await websockets.serve(handle_client, config.server_ip, config.server_port)
+    server = await websockets.serve(
+        handle_client,
+        config.server_ip,
+        config.server_port,
+        ping_timeout=60,
+        ping_interval=10
+    )
     await server.wait_closed()
 
 if __name__ == '__main__':
